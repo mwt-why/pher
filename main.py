45ea584111687c20ebb94e52980b18c2fa48ec25
WHY <1757751022@qq.com>
2023-02-13 15:27:10 +0800

    修改之前错误ocr方法

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
+import json
+
 from flask import Flask
 from flask import request
+from flask import jsonify
 from paddleocr import PaddleOCR
 import base64
 
 app = Flask(__name__)
-ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
+paddle_ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     return '<h1>this is a photo handle server</h1>'
 
 
-@app.route('/ocr')
+@app.route("/ocr", methods=['POST'])
 def ocr():
-    content = request.form['content']
+    data = json.loads(request.data)
+    content = data['data']
     img = base64.b64decode(content)
-    ocr_results = ocr.ocr(img, cls=True)
+    ocr_results = paddle_ocr.ocr(img, cls=True)
     response = []
     for orc_result in ocr_results:
-        response.append(
-            {orc_result[1][0], [orc_result[0][0][0], orc_result[0][0][1], orc_result[0][2][0], orc_result[0][2][1]]})
-    return response
+        label = orc_result[1][0]
+        box_x1 = orc_result[0][0][0]
+        box_y1 = orc_result[0][0][1]
+        box_x2 = orc_result[0][2][0]
+        box_y2 = orc_result[0][2][1]
+        response.append({'label': label, 'box': [box_x1, box_y1, box_x2, box_y2]})
+    return jsonify(response)
 
 
 if __name__ == '__main__':
